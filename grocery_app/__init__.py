059227202ffbeb07946d850e644989d879b7b7be
ruot-nyak <nyak.g.ruot@gmail.com>
2021-03-03 22:39:33 -0800

    created login using flask_login

--- grocery_app/__init__.py
+++ grocery_app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
-from grocery_app.config import Config
 import os
+from grocery_app.config import Config
+from grocery_app.routes import main
+from .models import User
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -9,9 +13,22 @@ app.secret_key = os.urandom(24)
 
 db = SQLAlchemy(app)
 
-from grocery_app.routes import main
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+bcrypt = Bcrypt(app)
+
+
+from grocery_app.routes import main, auth
 
 app.register_blueprint(main)
+app.register_blueprint(auth)
 
 with app.app_context():
     db.create_all()
--- grocery_app/models.py
+++ grocery_app/models.py
@@ -2,6 +2,7 @@ from sqlalchemy_utils import URLType
 
 from grocery_app import db
 from grocery_app.utils import FormEnum
+from flask_login import UserMixin
 
 class ItemCategory(FormEnum):
     """Categories of grocery items."""
@@ -34,7 +35,7 @@ class GroceryItem(db.Model):
     created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     created_by = db.relationship('User')
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(24), nullable=False, unique=True)
     password = db.column(db.String(80), nullable=False)
