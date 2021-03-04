afc05e5242e41b3a621e0946cb302d8d1f4ea5a0
ruot-nyak <nyak.g.ruot@gmail.com>
2021-03-03 22:24:29 -0800

    added users to models.py

--- grocery_app/models.py
+++ grocery_app/models.py
@@ -18,6 +18,8 @@ class GroceryStore(db.Model):
     title = db.Column(db.String(80), nullable=False)
     address = db.Column(db.String(200), nullable=False)
     items = db.relationship('GroceryItem', back_populates='store')
+    created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_by = db.relationship('User')
 
 class GroceryItem(db.Model):
     """Grocery Item model."""
@@ -29,3 +31,10 @@ class GroceryItem(db.Model):
     store_id = db.Column(
         db.Integer, db.ForeignKey('grocery_store.id'), nullable=False)
     store = db.relationship('GroceryStore', back_populates='items')
+    created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_by = db.relationship('User')
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(24), nullable=False, unique=True)
+    password = db.column(db.String(80), nullable=False)
